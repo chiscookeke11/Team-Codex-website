86c926cad4f12d77b10daa5bb4f67bc4355a432d
chiscookeke11 <chiscookeke11@gmail.com>
2025-01-30 19:16:06 -1100

    ADD: navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Hero = () => {
   return (
-    <div  className='font-outfit hero w-full h-full min-h-[79vh] flex item-stretch justify-left relative '>
+    <div  className='font-outfit hero w-full h-full min-h-[81vh] flex item-stretch justify-left relative '>
+      <Navbar/>
        <div className='overlay h-auto w-[50%] min-w-[340px] max-w-[853px] rounded-tr-[50px] '  > </div>  
        <div className='text absolute top-[50%] left-[7%]  translate-y-[-50%] text-white  '> 
-        <h1 className='font-bold text-7xl   ' > Innovating today <span className='text-[#24A556] ' >  {"{ }"}</span> {" "} <br/>
+        <h1 className='font-bold text-4xl md:text-6xl lg:text-7xl   ' > Innovating today <span className='text-[#24A556] ' >  {"{ }"}</span> {" "} <br/>
         for a smarter tomorrow</h1>
         </div> 
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useRef, useState } from 'react'
+import searchIcon from '../assets/search-icon.svg';
+import { BiMenu } from 'react-icons/bi';
+import { FiX } from 'react-icons/fi';
+
+
+
+const Navbar = () => {
+
+    const [showNavLinks, setShowNavLinks] = useState(false)
+    const navRef = useRef(null);
+
+
+
+
+    const navLinks = [
+        {
+            name: "About",
+            url: "#"
+        },
+        {
+            name: "Project",
+            url: "#"
+        },
+        {
+            name: "Service",
+            url: "#"
+        },
+        {
+            name: "Client",
+            url: "#"
+        },
+        {
+            name: "Team",
+            url: "#"
+        },
+        {
+            name: "Contact",
+            url: "#"
+        },
+    ]
+
+    const showMenu = () => {
+        setShowNavLinks((prev) => !prev)
+    }
+    const closeMenu =() => {
+        setShowNavLinks(false)
+    }
+    const handleClickOutside =  (event) => {
+        if (navRef.current && !navRef.current.contains(event.target)){
+            setShowNavLinks(false)
+        }
+    }
+
+    useEffect(()=> {
+        if (showNavLinks) {
+            document.addEventListener("mousedown", handleClickOutside)
+        }
+        else{
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        };
+    }, [showNavLinks])
+
+
+
+    return (
+        <nav ref={navRef} className='fixed z-[9999] w-[95%] max-w-[1320px] min-h-[60px] h-auto md:max-h-[90px] bg-white rounded-[9px] py-5 flex items-center justify-center top-[5%] left-[50%] transform translate-x-[-50%] ' >
+
+
+
+            <button onClick={showMenu} className={`absolute block md:hidden cursor-pointer   left-[4%] transform  text-[#3F3F3F] ${showNavLinks ? "top-[5%] " : "top-[50%] translate-y-[-50%] "} `}>
+                {showNavLinks ? <FiX size={35} /> : <BiMenu size={35} />}
+            </button>
+
+
+
+            <ul className={`my-7 md:flex md:flex-row flex-col items-center justify-center gap-5 md:gap-10  ${showNavLinks ? "flex" : "hidden"}  `} >
+                {navLinks.map((navLink, index) => (
+                    <li onClick={closeMenu} key={index} className='px-2 py-5 text-[#3F3F3F]  font-normal text-base lg:text-[15px] cursor-pointer hover:text-[#60CE70] transition duration-300 ease-in-out  ' > {navLink.name} </li>
+                ))}
+            </ul>
+
+
+
+
+            <img src={searchIcon} alt="search-icon" className={`absolute cursor-pointer  md:top-[50%] right-[4%] transform md:translate-y-[-50%] text-[#3F3F3F] ${showNavLinks ? "top-[5%]" : "top-[50%] translate-y-[-50%] "} `} />
+        </nav>
+    )
+}
+
+export default Navbar
