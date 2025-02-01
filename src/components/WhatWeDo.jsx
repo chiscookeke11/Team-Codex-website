aa5daaf011ce8d6bfae221b18d023059d9b5d1c8
chiscookeke11 <chiscookeke11@gmail.com>
2025-02-01 10:48:49 -1100

    ADD: what we can do

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -4,17 +4,36 @@ import curlyShape from '../assets/curlt.svg'
 
 const WhatWeDo = () => {
   return (
-    <div className='relative w-full mx-auto max-w-[1320px] h-[451px] bg-[#3F3F3F] rounded-[9px]  ' >
+    <div className='relative w-full mx-auto max-w-[1320px] h-[451px] bg-[#3F3F3F] rounded-[9px] pl-[10%] flex items-center justify-between  ' >
 
-        <div>
-            
+        <div className=' z-10 flex flex-col items-center gap-0.5 justify-center text-[#D7D7D7] font-semibold text-6xl ' >
+          <span className='ml-[10px] ' >what we</span>
+          <span className='ml-[-30%] ' >can do</span>
+          <span className='ml-[-30%] '>for you</span>
         </div>
+
+
+
+        <div className='bg-red-800 w-md'>
+          <div></div>
+
+          <div className='text-[#6D6D6D] font-normal text-3xl flex flex-col gap-3 cursor-pointer ' >
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Product Design</h2> <img src={curlyShape} alt="open" className='group-hover:visible invisible '  /> </div>
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Ideation & Strategy</h2> <img src={curlyShape} alt="open" className='group-hover:visible invisible '  /> </div>
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Web and Mobile</h2> <img src={curlyShape} alt="open" className='group-hover:visible invisible '  /> </div>
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Ai and Blockchain integration</h2> <img src={curlyShape} alt="open" className='group-hover:visible invisible '  /> </div>
+         
+          </div>
+
+        </div>
+           
 
 
 
 
         
-        <img src={grayBg} alt="bg" className='absolute top-0 bottom-0 left-[17%] h-full object-cover ' />
+       
+        <div className='h-full absolute w-fit  top-0 bottom-0 left-[17%]   ' > <img src={grayBg} alt="bg" className='h-full object-cover z-0  ' /> </div>
         <img src={curlyShape} alt="bg" className='absolute top-5 left-0 lg:w-[189px] w-16 md:w-20  object-cover ' />
 
         </div>
