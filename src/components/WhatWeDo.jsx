5f86c3b08250dce8868bf88768e5527a45453241
chiscookeke11 <chiscookeke11@gmail.com>
2025-02-02 21:56:12 -1100

    fixed

--- src/components/WhatWeDo.jsx
+++ src/components/WhatWeDo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import grayBg from '../assets/gray-bg.svg'
 import curlyShape from '../assets/curlt.svg'
+import curlyPointer from '../assets/curlyPointer.svg'
 import CustomStarIcon from './customStarIcon'
 
 const WhatWeDo = () => {
@@ -15,15 +16,16 @@ const WhatWeDo = () => {
         </div>
 
 
+        <div className=' min-w-md flex items-center justify-center'>
+          <div > 
+            <img src={curlyPointer} alt="curly-arrow" />
+          </div>
 
-        <div className=' w-md'>
-          <div></div>
-
-          <div className='text-[#6D6D6D] font-normal text-3xl flex flex-col gap-3 cursor-pointer ' >
-            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Product Design</h2> <CustomStarIcon className="invisible group-hover:visible" /> </div>
-            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Ideation & Strategy</h2><CustomStarIcon className="invisible group-hover:visible"/> </div>
-            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Web and Mobile</h2><CustomStarIcon className="invisible group-hover:visible"/> </div>
-            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%] hover:text-white transition duration-300 ease-in-out group ' >   <h2>Ai and Blockchain integration</h2> <CustomStarIcon className="invisible group-hover:visible"/></div>
+          <div className='text-[#6D6D6D] font-normal text-3xl flex flex-col gap-3 cursor-pointer basis-3/4 ' >
+            <div className=' py-3 border-b border-[#636060] flex items-center text-white justify-between pr-[13%] transition duration-300 ease-in-out  ' >   <h2>Product Design</h2> <CustomStarIcon/> </div>
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%]  transition duration-300 ease-in-out  ' >   <h2>Ideation & Strategy</h2> </div>
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%]  transition duration-300 ease-in-out ' >   <h2>Web and Mobile</h2> </div>
+            <div className=' py-3 border-b border-[#636060] flex items-center justify-between pr-[13%]  transition duration-300 ease-in-out whitespace-nowrap ' >   <h2>Ai and Blockchain integration</h2></div>
          
           </div>
 
@@ -35,7 +37,7 @@ const WhatWeDo = () => {
 
         
        
-        <div className='h-full absolute w-fit  top-0 bottom-0 left-[17%]   ' > <img src={grayBg} alt="bg" className='h-full object-cover z-0  ' /> </div>
+        <div className='h-full absolute w-fit  top-0 bottom-0 left-[17%]  ' > <img src={grayBg} alt="bg" className='h-full object-cover z-0 opacity-[50%]  '  /> </div>
         <img src={curlyShape} alt="bg" className='absolute top-5 left-0 lg:w-[189px] w-16 md:w-20  object-cover ' />
 
         </div>
