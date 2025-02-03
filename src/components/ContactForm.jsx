7377e0138b6256b3880c6057f9b29eb20910bd3f
chiscookeke11 <chiscookeke11@gmail.com>
2025-02-03 04:46:36 -1100

    test push

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div className='w-full px-5 py-5 ' >ContactForm</div>
+  )
+}
+
+export default ContactForm
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import AboutUs from '../components/AboutUs'
 import Projects from '../components/Projects'
 import WhatWeDo from '../components/WhatWeDo'
 import Footer from '../components/Footer'
+import ContactForm from '../components/ContactForm'
 
 const HomePage = () => {
   return (
@@ -13,6 +14,7 @@ const HomePage = () => {
         <div className='content w-full h-auto bg-[#e8e8e8] flex flex-col items-center justify-center gap-28  py-5 ' >
           <Projects/>
           <WhatWeDo/>
+          <ContactForm/>
         </div>
         <Footer/>
     </div>
